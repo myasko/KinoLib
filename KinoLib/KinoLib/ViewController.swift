f24eda5be6de3ec2f04d0c1b077c3cf5ff77361b
George <81197824+myasko@users.noreply.github.com>
2021-11-01 20:42:35 +0300

    Add files via upload

---
+++ KinoLib/KinoLib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KinoLib
+//
+//  Created by Георгий Бутров on 01.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
