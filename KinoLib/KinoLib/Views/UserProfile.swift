5c641fda57cfc33b72018ee06e6ea1e95d5307fe
KoroLion <korolion31@yandex.ru>
2021-12-17 07:55:57 +0300

    basic favorites info

--- KinoLib/KinoLib/Views/UserProfile.swift
+++ KinoLib/KinoLib/Views/UserProfile.swift
@@ -13,6 +13,7 @@ class UserProfileViewController: BaseViewController {
     let margin = 15.0
     let bottomMargin = 50.0
     
+    var favoritesLabel: UILabel!
     var logoutButton: UIButton!
     
     override func loadView() {
@@ -22,23 +23,52 @@ class UserProfileViewController: BaseViewController {
     }
     
     func setupView() {
-        guard let user = Auth.auth().currentUser else {
-              return
-        }
-        
-        title = user.email
+        favoritesLabel = createLabel(text: "Избранное: ")
+        favoritesLabel.numberOfLines = 0
+        contentView.addSubview(favoritesLabel)
         
         logoutButton = createButton(title: "Выйти")
         logoutButton.addTarget(self, action: #selector(logoutButtonAction), for: .touchUpInside)
         contentView.addSubview(logoutButton)
         
         NSLayoutConstraint.activate([
+            favoritesLabel.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: margin),
+            favoritesLabel.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: -margin),
+            favoritesLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: margin),
+            
             logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logoutButton.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: margin),
+            logoutButton.topAnchor.constraint(equalTo: favoritesLabel.topAnchor, constant: margin * 5),
             logoutButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
             
             logoutButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -bottomMargin),
         ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let user = Auth.auth().currentUser else {
+              return
+        }
+        
+        title = user.email
+        
+        DB.getFavoritesArr(user.uid) {
+            arr, err in
+            
+            if (err != nil) {
+                return
+            }
+            
+            var newText = "Избранное: "
+            for filmId in arr {
+                newText += String(filmId) + ", "
+            }
+            newText = String(newText.dropLast(2))
+            
+            self.favoritesLabel.text = newText
+            self.favoritesLabel.sizeToFit()
+        }
     }
     
     @objc func logoutButtonAction(sender: UIButton!) {
